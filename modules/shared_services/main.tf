ba15c23793e30d80dc163e8fc3e21448fe824896
tiltaslifestyle <vkurilovskyi@gmail.com>
2026-06-15 14:04:13 +0200

    feat: add azure log analytics workspace for centralized monitoring

--- modules/shared_services/main.tf
+++ modules/shared_services/main.tf
@@ -9,3 +9,10 @@ resource "random_string" "suffix" {
   special = false
 }
 
+resource "azurerm_log_analytics_workspace" "main" {
+  name                = "log-${var.application_name}-${var.environment}"
+  location            = azurerm_resource_group.shared.location
+  resource_group_name = azurerm_resource_group.shared.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
